54b11bd0e59a483025eb575bd70eda6608185657
zalulw <polyakzalan394@gmail.com>
2022-10-05 07:43:01 +0200

    valami

---
+++ 01 - Kiírás másolata/Feladat 01/main.py
@@ -0,0 +1,4 @@
+modell: str="Torino Cobra SportsRoof"
+gyartasiEv: int=1970
+
+print(f"A ford autógyár {gyartasiEv}-ben gyártotta le a Torino Cobra SportsRoof modellt.")
---
+++ 01 - Kiírás másolata/Feladat 02/main.py
@@ -0,0 +1,5 @@
+modell: str="i7"
+tipusSzam: int=8700
+munkaFrekvencia: float=4.2
+
+print(f"Az INTEL {modell} sorozatjelzésű mikroprocesszor {tipusSzam} típusának munkafrekvenciája {munkaFrekvencia}GHz.")
