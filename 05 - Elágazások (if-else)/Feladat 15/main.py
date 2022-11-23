67f51491fd2b6f4f8cd73ff83c96a259fb1d2505
zalulw <115061913+zalulw@users.noreply.github.com>
2022-11-23 07:49:09 +0100

    20221123

--- 05 - Elágazások (if-else)/Feladat 15/main.py
+++ 05 - Elágazások (if-else)/Feladat 15/main.py
@@ -16,37 +16,69 @@ gyumolcs: bool = False
 sultkrumpli: bool = False
 salata: bool = False
 kola: bool = False
-
-eloetel: str = None
-foetel: str = None
-koret: str = None
-
-valtozas: int = None
+#valasztas
+eloetel: int = None
+foetel: int = None
+koret: int = None
 
 
 print("a mai menü: \n Előételek: \n Zoldsegleves [1] \n Husleves [2] \n Gyumolcsleves [3]")
-valtozas=int(input())
-if (valtozas == 1):
+eloetel=int(input())
+if (eloetel == 1):
     zoldsegleves:bool = True
 
-elif(valtozas == 2):
+elif(eloetel == 2):
     husleves:bool=True
 
-elif(valtozas == 3):
+elif(eloetel == 3):
     gyumolcsleves:bool=True
 
-
-
 print("Főételek: \n Csirkecomb [1] \n Csirkemell [2] \n Rakottzoldseg [3] \n spagetti [4] \n pizza [5]")
-foetel=str(input())
+foetel=int(input())
+if (foetel == 1):
+    csirkecomb:bool=True
 
+elif (foetel == 2):
+    csirkemell:bool=True
 
+elif (foetel == 3):
+    rakottzoldseg:bool=True
+
+elif (foetel == 4):
+    spagetti:bool=True
+
+elif (foetel == 5):
+    pizza:bool=True
 
 print("Köretek: \n Rizs [1]\n Paroltzoldseg [2]\n Gyumolcs [3]\n Sultkrumpli [4]\n Salata [5]\n Kola [6]")
-koret=str(input())
+koret=int(input())
+if (koret == 1):
+    Rizs:bool=True
+
+elif (koret == 2):
+    paroltzoldseg:bool = True
+
+elif (koret == 3):
+    gyumolcs:bool=True
+
+elif (koret == 4):
+    sultkrumpli:bool=True
+
+elif (koret == 5):
+    salata:bool=True
+
+elif (koret == 6):
+    kola:bool = True
 
 
+if (zoldsegleves and spagetti and (gyumolcs or salata) and (pizza != True and rakottzoldseg != True)):
+    print("kivalo ertekelesu menu")
 
+elif (zoldsegleves and csirkemell and rizs and sultkrumpli != True):
+    print("fitnessz menu")
 
+elif (husleves and csirkecomb and (sultkrumpli and salata) and (pizza != True and rakottzoldseg != True)):
+    print("vasarnapi menu")
 
-
+elif ((pizza or spagetti) and (gyumolcs or kola) and (rakottzoldseg != True and paroltzoldseg != True)):
+    print("napi menu") 
