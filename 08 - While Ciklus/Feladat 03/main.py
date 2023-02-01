0e5ee3083c046c3d8bc8d1adf25171f6657786db
zalulw <115061913+zalulw@users.noreply.github.com>
2023-02-01 07:53:36 +0100

    20230201

---
+++ 08 - While Ciklus/Feladat 03/main.py
@@ -0,0 +1,19 @@
+from os import system
+import random
+
+randomNum = random.randint(0, 9)
+num: int = None
+tries: int = 0
+
+print("0 es 9 kozti szamot talalja ki 5 probabol!", end='')
+
+while(tries < 5 and num != randomNum):
+    print("rookie mistake baby bozo 🤡🤡🤡", end='')
+    num=int(input())
+    tries += 1
+    if(tries == 5):
+        print("vege a mokanak kis krapek ocsipok ocskos")
+        break
+
+    if(num == randomNum):
+        print("gratulalok 🎉🎉🎉🎉🎉🎉")
