3f3b3e50cfd5fa7585072b0a74590cde1a084ae9
zalulw <115061913+zalulw@users.noreply.github.com>
2023-03-08 07:23:25 +0100

    20230307

--- 09 - Függvények/Feladat 01/main.py
+++ 09 - Függvények/Feladat 01/main.py
@@ -11,3 +11,12 @@ y = getNumberFromConsole()
 
 result = sumOfTwo(x, y)
 printToConsole(x, y, result, "+")
+
+result = multipleTwo(x, y)
+printToConsole(x, y, result,  "*")
+
+result = divideTwo(x, y)
+printToConsole(x, y, result, "/")
+
+result = substractTwo(x, y)
+printToConsole(x, y, result, "-")
--- 09 - Függvények/Feladat 01/mathfunctions.py
+++ 09 - Függvények/Feladat 01/mathfunctions.py
@@ -3,3 +3,19 @@ def sumOfTwo(a: float, b: float)->float:
     result = a + b
 
     return result
+
+def multipleTwo(a: float, b: float)->float:
+    result:float = None
+    result = a * b
+
+    return result
+
+def divideTwo(a: float, b:float)->float:
+    result:float = None
+    result = a / b
+
+    return result
+
+def substractTwo(a: float, b:float)->float:
+    result:float = None
+    result = a - b
