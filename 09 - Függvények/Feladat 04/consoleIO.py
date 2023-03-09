298af54e138ed8c8b1dc6ecac8a33af8c9a1409d
zalulw <polyakzalan394@gmail.com>
2023-03-09 17:26:04 +0100

    20230309

--- 09 - Függvények/Feladat 04/consoleIO.py
+++ 09 - Függvények/Feladat 04/consoleIO.py
@@ -11,3 +11,6 @@ def getNamefromConsole()->str:
 
     return name.strip().capitalize()
 
+def color(s: str)->str:
+    value: int = len(s)
+    return f"\033[38;2;{value};{value};{value}m{s}\033[0m"
--- 09 - Függvények/Feladat 04/main.py
+++ 09 - Függvények/Feladat 04/main.py
@@ -1,2 +1,8 @@
 from os import system
 from consoleIO import *
+
+name: str = None
+
+name: str = getNamefromConsole()
+name = color(name)
+print(f"Üdvözlöm, {name}")
